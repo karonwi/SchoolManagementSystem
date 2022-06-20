ff3d65565548a869f09b07dd4b0abe13c25102b5
karonwi <iyinoluwa@thankucash.com>
2022-06-20 19:23:48 +0100

    Interfaces written

--- SchoolManagement.Core/Interfaces/ITokenGenerator.cs
+++ SchoolManagement.Core/Interfaces/ITokenGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SchoolManagement.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Core.Interfaces
 {
-    internal class ITokenGenerator
+    public interface ITokenGenerator
     {
+        public Task<string> GenerateTokenAsync(User user);
     }
 }
--- SchoolManagement.Infrastructure/Services/AuthenticationService.cs
+++ SchoolManagement.Infrastructure/Services/AuthenticationService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using SchoolManagement.Core.DTOs;
+using SchoolManagement.Core.Entities;
+using SchoolManagement.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,59 @@ using System.Threading.Tasks;
 
 namespace SchoolManagement.Infrastructure.Services
 {
-    internal class AuthenticationService
+    public class AuthenticationService : IAuthenticationService
     {
+        private readonly UserManager<User> _userManager;
+        public AuthenticationService(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+    
+        public Task<Response<string>> EmailConfirmationAsync(ConfirmEmailRequestDTO emailRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<string>> ForgotPasswordAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<UserResponseDTO>> LoginAsync(UserRequestDTO userRequest)
+        {
+            var findUser = await _userManager.FindByEmailAsync(userRequest.Email);
+            if (findUser != null)
+            {
+                if (await _userManager.CheckPasswordAsync(findUser,userRequest.Password))
+                {
+                    if (findUser.EmailConfirmed)
+                    {
+                        return new Response<UserResponseDTO>
+                        {
+                            Data = new UserResponseDTO
+                            {
+                                Email = $"The email {userRequest.Email} has been confirmed",
+                                FullName = findUser.FirstName + " " + findUser.LastName,
+                                Id = findUser.Id,
+                                Token = 
+                            }
+                        }
+                    }
+                }
+                throw new AccessViolationException("Invalid Password");
+                
+            }
+            throw new AccessViolationException("Invalid Credentials");
+        }
+
+        public Task<Response<string>> ResetPasswordAsync(ResetPasswordDTO resetPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<string>> UpdatePasswordAsync(UpdatePasswordDTO updatePassword)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
